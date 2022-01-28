730d9dc228c9b9232cbe6f7b8298159bf5752f3d
Darren Cobian <darrencobian98@gmail.com>
2022-01-28 11:51:51 -0800

    Inserting into a binary tree

--- data-structures/binary-trees/basic_binary_tree.cpp
+++ data-structures/binary-trees/basic_binary_tree.cpp
@@ -1,24 +1,90 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 struct Node {
     int data;
     struct Node* left;
     struct Node* right;
-
-    // val is the key or value that has to be added to the data part
-    Node(int val) {
-        data = val;
-        // left and right child for node will be initialized to NULL
-        left = NULL;
-        right = NULL;
-    }
 };
 
+/* Function to create a new node */
+Node* createNode(int data) {
+    Node* newNode = new Node();
+
+    if(!newNode) {
+        cout << "Memory error" << endl;
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+/* Function to insert into a binary tree */
+Node* insertNode(Node* root, int data) {
+    // if the tree is empty, create a new node
+    if(root == NULL) {
+        root = createNode(data);
+        return root;
+    }
+
+    // else do a level order traversal until we find an empty place
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp->left != NULL) {
+            q.push(temp->left);
+        }
+        else {
+            temp->left = createNode(data);
+            return root;
+        }
+
+        if(temp->right != NULL) {
+            q.push(temp->right);
+        }
+        else {
+            temp->right = createNode(data);
+            return root;
+        }
+    }
+}
+
+/* Inorder traversal of a binary tree */
+void inOrder(Node* temp) {
+    if(temp == NULL) {
+        return;
+    }
+
+    inOrder(temp->left);
+    cout << temp->data << ' ';
+    inOrder(temp->right);
+}
+
 int main() {
-    struct Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
+    Node* root = createNode(10);
+    root->left = createNode(11);
+    root->left->left = createNode(7);
+    root->right = createNode(9);
+    root->right->left = createNode(15);
+    root->right->right = createNode(8);
+    
+    cout << "Inorder traversal before insertion: ";
+    inOrder(root);
+    cout << endl;
+
+    int key = 12;
+    root = insertNode(root, key);
+
+    cout << "Inorder traversal after insertion: ";
+    inOrder(root);
+    cout << endl;
+
     return 0;
 }
