070339f8792d9bcc41f8d6de0aba0415be59b80d
Darren Cobian <darrencobian98@gmail.com>
2022-01-30 22:37:23 -0800

    reverse a null terminated string

--- practice/reverseNullStr.cpp
+++ practice/reverseNullStr.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
+/*
+    Implement a function void reverse(char* str) in C or C++ which reverses
+    a null-terminated string.
+*/
+
+// when using a null terminated string, *var prints the first letter of the 
+// array and !*var returns true if the string is empty
 void reverse(char* str) {
-    int size = sizeof(str)/sizeof(str[0]);
-    cout << size << endl;
+    if(!*str) {
+        return;
+    }
+
+    // save the beginning of the string
+    char* begin = str;
+    
+    // point at the last character
+    char* end = begin + strlen(str) - 1;
+    
+    // swap begin and end
+    while(begin < end) {
+        char c = *begin;
+        *begin = *end;
+        *end = c;
+        begin++;
+        end--;
+    }
 }
 
 int main() {
-    char str[] = "hi";
-    reverse(str);
+    char strArr[] = "1point3acres";
+    cout << "Before Reverse: " << strArr << endl;
+    reverse(strArr);
+    cout << "After Reverse: " << strArr << endl;
     return 0;
 }
